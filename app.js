0ba84c86495fd001edbbccdd906d492c96738480
David Zhang <David@Davids-MacBook-Pro.local>
2015-04-10 18:11:12 -0700

    Finished DVD Search with Node Homework

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express'); 
+var ejs = require('ejs'); 
+var Dvd = require('./models/Dvd'); 
+
+var app = express(); 
+app.set('view engine', 'ejs'); 
+
+app.get('/', function(req, res) { 
+	res.render('index', {
+		title: 'Home'
+	});
+});
+
+app.get('/dvds', function(req, res) {
+	console.log(req.query.title); 
+
+	Dvd.findAll({
+		where: { 
+			title: { like: '%' + req.query.title + '%' },
+			award: req.query.award
+		}
+	}).then(function(results) {
+		res.render('dvds', {
+			title: 'Dvd Search Results',
+			dvds: results
+		});
+	});
+});
+
+app.listen(3000, function() {
+	console.log('Listening on localhost:3000');
+});
+
